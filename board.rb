a845ff2769d90b617190bed37aa5839fca30c6bb
macla7 <mitchclarkmapie@hotmail.com>
2021-02-04 07:48:18 +1100

    1x knight and rook perfect

--- board.rb
+++ board.rb
@@ -1,6 +1,6 @@
 # Make proper grid, and self.allowed?(move)
 class Chessboard
-  attr_accessor :board
+  attr_accessor :board, :black, :white
   def initialize
     @board = {}
     for i in 1..8
@@ -8,6 +8,8 @@ class Chessboard
         @board["#{i}, #{j}"] = ' '
       end
     end
+    @black = ["\u{265A}", "\u{265B}", "\u{265C}", "\u{265D}", "\u{265E}", "\u{265F}"]
+    @white = ["\u{2654}", "\u{2655}", "\u{2656}", "\u{2657}", "\u{2658}", "\u{265F9}"]
   end
 
   def allowed?(move)
@@ -17,15 +19,15 @@ class Chessboard
   end
 
   def print_board
-    8.downto(1) do |i|
+    puts '-----------------------------------'
+    8.downto(1) do |j|
       print '|'
-      for j in 1..8
+      for i in 1..8
         print " #{@board["#{i}, #{j}"]} |"
       end
       puts "\n"
     end
   end
-
 end
 
 # Nodes for the path
--- rook.rb
+++ rook.rb
@@ -4,14 +4,17 @@ require './new_knight.rb'
 # moves, incoporating possible moves using Chessboard.allowed?
 class Rook
   attr_reader :possible, :game, :pos
-  def initialize(game, pos = [1,1])
+  def initialize(game, pos = [1,1], colour)
     @pos = pos
     @possible = []
     @game = game
+    @piece_type = '\u{2656}'
     game.board["#{pos[0]}, #{pos[1]}"] = "\u{2656}"
+    @colour = colour
+    name = 'WR1'
   end
 
-  def move_piece(end_pos)
+  def move_piece(end_pos, pos = @pos)
     possible_moves(game)
     if @possible.include?(end_pos)
       place(end_pos)
@@ -20,53 +23,51 @@ class Rook
     end
   end
 
-  def place(pos)
-    game.board["#{pos[0]}, #{pos[1]}"] = "\u{2656}"
-    @pos = pos
+  def place(end_pos)
+    game.board["#{pos[0]}, #{pos[1]}"] = ' '
+    game.board["#{end_pos[0]}, #{end_pos[1]}"] = "\u{2656}"
+    @pos = end_pos
   end
 
-  def possible_moves(board, pos = @pos)
-    possible_left_right(board, pos)
-    possible_up_down(board, pos)
-    copy = @possible
+  def possible_moves(game, pos = @pos)
     @possible = []
-    copy
+    possible_left_right(game, pos)
+    possible_up_down(game, pos)
+    @possible
   end
 
-  def possible_left_right(board, pos = @pos)
+  def possible_left_right(game, pos = @pos)
     # Going to need some kind of recurision here I think.
     for i in [1, -1]
-      a = "#{pos[0]+i}, #{pos[1]}"
       b = [pos[0]+i, pos[1]]
-      unless board.board[a].nil?
-        if !board.board[a].match(/^W/) && @possible.none?(b)
-          @possible.push(b) if board.allowed? b
-          unless board.board[a].match(/^B/)
-            possible_left_right(board, b) if board.allowed? b
+      unless game.board["#{pos[0]+i}, #{pos[1]}"].nil?
+        if !game.white.include?(@piece_type) && @possible.none?(b)
+          @possible.push(b) if game.allowed? b
+          unless game.black.include?(@piece_type)
+            possible_left_right(game, b) if game.allowed? b
           end
         end
       end
     end
   end
 
-  def possible_up_down(board, pos = @pos)
+  def possible_up_down(game, pos = @pos)
     for i in [1, -1]
-      a = "#{pos[0]}, #{pos[1]+i}"
       b = [pos[0], pos[1]+i]
-      unless board.board[a].nil?
-        if !board.board[a].match(/^W/) && @possible.none?(b)
-          @possible.push(b) if board.allowed? b
-          unless board.board[a].match(/^B/)
-            possible_up_down(board, b) if board.allowed? b
+      unless game.board["#{pos[0]}, #{pos[1]+i}"].nil?
+        if !game.white.include?(@piece_type) && @possible.none?(b)
+          @possible.push(b) if game.allowed? b
+          unless game.black.include?(@piece_type)
+            possible_up_down(game, b) if game.allowed? b
           end
         end
       end
     end
   end
 
-  def rook_moves(board, end_pos)
+  def rook_moves(game, end_pos)
     the_way = Path.new(@pos)
-    the_way.path_to(board, end_pos, self)
+    the_way.path_to(game, end_pos, self)
     the_way.found_it
   end
 end
@@ -76,9 +77,11 @@ alex = Rook.new(game, [6,3])
 p alex.place([5,5])
 james = Knight.new(game, [5,6])
 dav = Knight.new(game, [6,5])
+game.print_board
 alex.move_piece([8,8])
 alex.rook_moves(game, [8,8])
 alex.move_piece([5,6])
+game.print_board
 alex.move_piece([5,8])
 game.print_board
 
